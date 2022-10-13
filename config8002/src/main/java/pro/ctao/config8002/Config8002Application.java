6c268dd2f960c1255581d66a5876ea469a6a7e91
zoutao98 <ezoutao@foxmail.com>
2022-10-13 22:31:11 +0800

    配置中心?

---
+++ config8002/src/main/java/pro/ctao/config8002/Config8002Application.java
@@ -0,0 +1,34 @@
+package pro.ctao.config8002;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Config8002Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(Config8002Application.class, args);
+		String userName = applicationContext.getEnvironment().getProperty("user.name");
+        String userAge = applicationContext.getEnvironment().getProperty("user.age");
+        System.err.println("user name :"+userName+"; age: "+userAge);
+	}
+
+	@RestController
+	@RequestMapping("/config")
+	@RefreshScope
+	public class ConfigController {
+
+		@Value("${useLocalCache:false}")
+		private boolean useLocalCache;
+
+		@RequestMapping("/get")
+		public boolean get() {
+			return useLocalCache;
+		}
+	}
+}
---
+++ config8002/src/test/java/pro/ctao/config8002/Config8002ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.ctao.config8002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Config8002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
