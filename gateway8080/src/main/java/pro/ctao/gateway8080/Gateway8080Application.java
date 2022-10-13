03718ce542f2bf93419d0cd4cf9eb0115c317526
zoutao98 <ezoutao@foxmail.com>
2022-10-13 21:38:28 +0800

    spring cloud gateway

---
+++ gateway8080/src/main/java/pro/ctao/gateway8080/Gateway8080Application.java
@@ -0,0 +1,15 @@
+package pro.ctao.gateway8080;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Gateway8080Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gateway8080Application.class, args);
+	}
+
+}
---
+++ gateway8080/src/test/java/pro/ctao/gateway8080/Gateway8080ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.ctao.gateway8080;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gateway8080ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
